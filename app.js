5d4c52b07b54f174f120a5e7985a298b5e6ffbce
dxm <yjqvip8@163.com>
2019-04-24 10:20:01 +0800

    去掉.md

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var btn = document.getElementsByTagName("button")[0]
 btn.onclick=function(){
     console.log("开始跳转")
-    window.location.href = "./novel.md";
+    window.location.href = "./novel";
 }
 console.log(btn)
